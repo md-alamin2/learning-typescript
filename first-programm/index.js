f7bf1c5c51d2d08183d834117e6cb9b4b0933edf
md-alamin2 <mdalamin22671@gmail.com>
2025-08-30 22:30:59 +0600

    learn how to run ts code and how to set type

---
+++ first-programm/index.js
@@ -0,0 +1,7 @@
+function sum(num1, num2) {
+    console.log(num1 + num2);
+}
+sum(10, 20);
+// How to run typeScript code?
+// step1: in terminal run "tsc index.ts" and make the file in javaScript
+// step2: run the javascript file with "node index.js"
---
+++ first-programm/index.ts
@@ -0,0 +1,9 @@
+function sum (num1: number, num2: number){
+    console.log(num1 + num2)
+}
+
+sum(10, 20)
+
+// How to run typeScript code?
+// step1: in terminal run "tsc index.ts" and make the file in javaScript
+// step2: run the javascript file with "node index.js"
