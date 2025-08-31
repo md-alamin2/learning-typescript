f8cd4d912e11c7d4b730ba79c782b5e2e0bdcc87
md-alamin2 <mdalamin22671@gmail.com>
2025-09-01 00:02:51 +0600

    learned about tuple-type

---
+++ user-defined-type/tuple-type.js
@@ -0,0 +1,5 @@
+// tuple type -> Tuple type is a special type of array that allows us to define the type of elements at specific positions in the array.
+var user;
+user = [1, "jon"];
+// user = ["jon", 1] // Error
+console.log(user);
---
+++ user-defined-type/tuple-type.ts
@@ -0,0 +1,8 @@
+// tuple type -> Tuple type is a special type of array that allows us to define the type of elements at specific positions in the array.
+
+let user : [number, string];
+
+user = [1, "jon"];
+// user = ["jon", 1] // Error
+
+console.log(user)
